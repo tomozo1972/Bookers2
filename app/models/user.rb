3b9eb5f5f263b0258cad15010c14f8a3473f9483
EC2 Default User <ec2-user@ip-172-31-38-69.ap-northeast-1.compute.internal>
2023-01-18 15:01:03 +0000

    [Add]バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :profile_image
   has_many :books, dependent: :destroy
 
-
-has_one_attached :profile_image
-
+validates :user_name, uniqueness: true, length: { in: 2..20 }
+validates :introduction, presence: true, length: { maximum: 50 }
 
 def get_profile_image(width, height)
   unless profile_image.attached?
